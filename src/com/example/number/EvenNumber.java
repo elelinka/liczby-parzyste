9f24d4d16d1c59e2e64ddc59958bb7ecb52d4e7c
Ewelina Gregoraszczuk <gregoraszczuk.e@gmail.com>
2019-07-25 09:27:36 +0200

    method has been changed

--- src/com/example/number/EvenNumber.java
+++ src/com/example/number/EvenNumber.java
@@ -4,11 +4,9 @@ public class EvenNumber {
 
     public void evenNumber() {
         int sum = 0;
-        for (int i = 0; i <= 100; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
-                sum += i;
-            }
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.print(i + " ");
+            sum += i;
         }
         System.out.println("\nSuma liczb: " + sum);
     }
